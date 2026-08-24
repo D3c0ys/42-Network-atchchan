0df57797577cac0859e3819016023832058d7f33
D3c0ys <viruspixel@protonmail.com>
2026-08-24 17:56:44 +0700

    Add Libft ft_isalpha

---
+++ Libft/ft_isalpha.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atchchan <atchchan@student.42bangkok.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/24 17:43:33 by atchchan          #+#    #+#             */
+/*   Updated: 2026/08/24 17:44:38 by atchchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//     isalpha()
+//            checks for an alphabetic character; in the standard "C" locale, it is  equivalent  to  (isupper(c) || islower(c)).
+//            In  some locales, there may be additional characters for which isalpha() is true——letters which are neither upper‐
+//            case nor lowercase.
+
+int ft_isalpha(int c);
+
+int ft_isalpha(int c){
+
+}
