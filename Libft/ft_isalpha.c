5a00f97d61f73a7521bec2d8387f25ee5a11cf24
D3c0ys <viruspixel@protonmail.com>
2026-08-25 22:38:43 +0700

    Add c_formatter_42 in flake.nix and update Libft

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,17 +6,17 @@
 /*   By: atchchan <atchchan@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/24 17:43:33 by atchchan          #+#    #+#             */
-/*   Updated: 2026/08/24 17:44:38 by atchchan         ###   ########.fr       */
+/*   Updated: 2026/08/25 22:25:01 by atchchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//     isalpha()
-//            checks for an alphabetic character; in the standard "C" locale, it is  equivalent  to  (isupper(c) || islower(c)).
-//            In  some locales, there may be additional characters for which isalpha() is true——letters which are neither upper‐
-//            case nor lowercase.
+int	ft_isalpha(int c);
 
-int ft_isalpha(int c);
-
-int ft_isalpha(int c){
-
+int	ft_isalpha(int c)
+{
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
 }
